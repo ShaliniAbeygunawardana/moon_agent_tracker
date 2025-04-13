29ceb21ad678c7405bd53ece41be256809b27a5c
ShaliniAbeygunawardana <shaliniimantha@gmail.com>
2025-04-13 16:38:22 +0530

    change the endpoint details to RDS

--- agent/configs.py
+++ agent/configs.py
@@ -1,8 +1,8 @@
 import os
 
-DB_USERNAME = os.getenv('DB_USERNAME', 'root')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'sha1014*')
-DB_ENDPOINT = os.getenv('DB_ENDPOINT', '127.0.0.1')
+DB_USERNAME = os.getenv('DB_USERNAME', 'admin')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'Sha1014*')
+DB_ENDPOINT = os.getenv('DB_ENDPOINT', 'moon-agent-database.cu76c40m8t8k.us-east-1.rds.amazonaws.com')
 DB_NAME = os.getenv('DB_NAME', 'moon_agent')
 DB_STRING_CHECK = os.getenv('DB_STRING')
 
